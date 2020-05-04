3456f10c33529b93de8e953bb998b4b56db4d1f0
Marek Chowaniok <marek.chowaniok@gmail.com>
2020-05-04 17:28:50 +0200

    runnning server

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleWare(protectedEndpoint)).Methods("GET")
+
+	log.Println("Listen oon port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Signup invoked")
+	w.Write([]byte("succesfully called signup"))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("login invoked")
+	w.Write([]byte("succesfully called login"))
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("protected invoked")
+	w.Write([]byte("succesfully called protectedEndpoint"))
+}
+
+// TokenVerifyMiddleWare function
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	fmt.Println("TokenVerifyMiddleWare invoked")
+	return nil
 }
